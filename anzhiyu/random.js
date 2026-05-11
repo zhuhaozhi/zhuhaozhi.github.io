c6ac685cc13e3fabb909cf867b86f1dd5a53baef
zhuhaozhi <941777577@qq.com>
2026-05-11 22:40:25 +0800

    Site updated: 2026-05-11 22:40:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/11/hello-world/"];function toRandomPost(){
+var posts=["hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
